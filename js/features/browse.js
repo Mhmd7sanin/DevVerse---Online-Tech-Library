a0cf01191d8548dd8d8e264d053dd45439ecf80a
Mohamed Hassanin <iihassanin4@gmail.com>
2026-04-12 20:20:59 +0200

    feat: merging feature browse to the main (#26)
    * add browse & details feature
    
    * modifying JS files
    
    * add heart img on browse
    
    * add the heart
    
    * modifying badge
    
    * design modification of book-detail
    
    * final modify
    
    * add toast & dialog
    
    * add toast &dialog
    
    * fix: fixed some issues in the read button logic and the responsive design for pages
    
    
    ---------
    
    Co-authored-by: MostafaHesham321 <20240603@stud.fci-cu.edu.eg>

---
+++ js/features/browse.js
@@ -0,0 +1,84 @@
+requireAuth(false);
+
+
+function initPage() {
+    const books = getBooks();
+    displayBooks(books);
+    document.getElementById('search-input').addEventListener('input', filterAndSearchBooks);
+
+    document.querySelectorAll('.filter-pill').forEach(button => {
+      button.addEventListener('click', () => {
+        document.querySelectorAll('.filter-pill').forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+        filterAndSearchBooks();
+      });
+    });
+}
+
+
+function displayBooks(filteredBooks) {
+      let user = getCurrentUser();
+
+      const grid = document.getElementById('books-grid');
+      grid.innerHTML = ''; 
+
+      filteredBooks.forEach(book => {
+
+        const bookCard = `
+          <a href="../../pages/user/book-detail.html?id=${book.id}" class="book-card">
+            <div class="book-card__cover">
+              <div class="book-card__image-wrapper">
+                <img class="book-img" src="${book.image || ''}">
+              <div class="book-card__fallback">❤</div>
+              <span class="book-card__avail ${
+                book.isAvailable ? 'book-card__avail--available':( book.borrowedBy == user.id ? 'book-card__avail--owned' :'book-card__avail--borrowed' )
+              }">${book.isAvailable ? 'AVAILABLED' : ( book.borrowedBy == user.id ? 'OWNED' :'BORROWED' )}</span>
+
+              </div>
+            </div>
+            <div class="book-card__body">
+              <p class="book-card__category">${book.category.toUpperCase()}</p>
+              <h3 class="book-card__title">${book.name}</h3>
+              <p class="book-card__author">${book.author}</p>
+            </div>
+          </a>
+        `;
+        grid.innerHTML += bookCard;
+      });
+
+      document.querySelectorAll('.book-img').forEach(img => {
+      img.addEventListener('error', function () {
+        this.style.display = 'none';
+        this.parentElement.querySelector('.book-card__fallback').style.display = 'flex';
+      });
+
+      if (!img.getAttribute('src')) {
+        img.style.display = 'none';
+        img.parentElement.querySelector('.book-card__fallback').style.display = 'flex';
+      }
+    });
+
+      if (filteredBooks.length === 0) {
+        grid.innerHTML = `<p class="no-books">No books found.</p>`;
+      }
+}
+
+function filterAndSearchBooks() {
+      const searchInput = document.getElementById('search-input').value.toLowerCase();
+      const activeCategory = document.querySelector('.filter-pill.active').id;
+      let books = getBooks();
+
+
+      if (activeCategory !== 'All') {
+        books = books.filter(book => book.category === activeCategory);
+      }
+
+      if (searchInput) {
+        books = books.filter(book =>
+          book.name.toLowerCase().includes(searchInput) ||
+          book.author.toLowerCase().includes(searchInput)
+        );
+      }
+
+      displayBooks(books);
+}
